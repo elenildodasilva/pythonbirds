97f13239d214fa269e2eda484183daf1cb73ab56
Elenildo <elenildo.silva@gmail.com>
2020-12-16 16:09:31 -0400

    criado o método cumprimentar

--- oo/pessoa2.py
+++ oo/pessoa2.py
@@ -1,10 +1,14 @@
 class Pessoa:
+    def __init__(self, nome=None):
+      self.nome = nome
     def cumprimentar(self):
         return f'olá {id(self)}'
 
 if __name__== '__main__':
-    p=Pessoa()
+    p=Pessoa('Elenildo')
     print(Pessoa.cumprimentar(p))
     print(id(p))
     print(p.cumprimentar())
-
+    print(p.nome)
+    p.nome='Elenildo'
+    print(p.nome)
