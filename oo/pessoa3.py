1c45cb85334c8d783ba54a84c49d053faf3cd1df
Elenildo <elenildo.silva@gmail.com>
2020-12-17 15:55:38 -0400

    criado atributo atributo dinamico

---
+++ oo/pessoa3.py
@@ -0,0 +1,22 @@
+  def __init__(self, *filhos, nome=None, idade=35):
+      self.nome = nome
+      self.idade = idade
+      self.filhos = list(filhos)
+
+
+    def cumprimentar(self):
+        return f'olá {id(self)}'
+
+if __name__== '__main__':
+    Pedro = Pessoa(nome='Pedro')
+    Elenildo = Pessoa(Pedro, nome='Elenildo')
+    print(Pessoa.cumprimentar(Elenildo))
+    print(id(Elenildo))
+    print(Elenildo.cumprimentar)
+    print(Elenildo.nome)
+    print(Elenildo.idade)
+    for filho in Elenildo.filhos:
+        print(filho.nome)
+    Elenildo.sobrenome = 'Silva'
+    print(Elenildo.__dict__)
+    print(Silva.__dict__)
