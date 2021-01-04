d8c0ccd679bef06f7d1dc870160ae027639e03a0
Elenildo <elenildo.silva@gmail.com>
2021-01-04 10:07:57 -0400

    criando objetos, classes e atributos implementada direção

--- oo/Carro.py
+++ oo/Carro.py
@@ -1,3 +1,5 @@
+
+
 """
 voce deve criar uma classe carro que vai possuir 2 atributos compostos por outras duas classes:
 motor
@@ -19,7 +21,7 @@ O     L
 
 Exemplo:
 >>> # Testando motor
->>> motor =motor()
+>>> motor = Motor()
 >>> motor.velocidade
 0
 >>> motor.acelerar()
@@ -38,7 +40,7 @@ Exemplo:
 >>> motor.velocidade
 0
 >>> # Testando direcao
->>> direcao.Direcao()
+>>> direcao = Direcao()
 >>> direcao.valor
 'Norte'
 >>> direcao.girar_a_direita()
@@ -91,4 +93,45 @@ Exemplo:
 >>> carro.girar_a_esquerda()
 >>> carro.calcular_a_direcao()
 >>> 'Oeste'
+"""
+class Motor:
+    def __init__(self):
+        self.velocidade=0
+    def acelerar(self):
+        self.velocidade+=1
+    def frear(self):
+        self.velocidade-=2
+        self.velocidade=max(0, self.velocidade)
+
+NORTE ='Norte'
+SUL   ='Sul'
+LESTE ='Leste'
+OESTE ='Oeste'
+
+class Direcao:
+    rotacao_a_direita_dct = {NORTE: LESTE, LESTE: SUL, SUL: OESTE, OESTE: NORTE}
+    rotacao_a_esquerda_dct = {NORTE: OESTE, OESTE: SUL, SUL: LESTE, LESTE: NORTE}
+
+    def __init__(self):
+        self.valor = NORTE
+    def girar_a_direita(self):
+        self.valor = self.rotacao_a_direita_dct[self.valor]
+    def girar_a_esquerda(self):
+        self.valor = self.rotacao_a_esquerda_dct[self.valor]
+
+
+"""
+
+ou
+
+        if self.valor == NORTE:
+           self.valor = LESTE
+        elif self.valor == LESTE:
+            self.valor = SUL
+        elif self.valor == SUL:
+            self.valor = OESTE
+        elif self.valor == SUL:
+            self.valor = OESTE
+        elif self.valor == OESTE:
+            self.valor = NORTE
 """
