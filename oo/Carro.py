71d9cbcca4f27438a34a92518b15fa53f26ee35d
Elenildo <elenildo.silva@gmail.com>
2021-01-04 10:52:58 -0400

    criando objetos, classes e atributos implementada direção

--- oo/Carro.py
+++ oo/Carro.py
@@ -81,18 +81,18 @@ Exemplo:
 0
 
 >>> carro.calcular_direcao()
->>> 'Norte'
+'Norte'
 >>> carro.girar_a_direita()
 >>> carro.calcular_direcao()
->>> 'Leste'
+'Leste'
 
 >>> carro.girar_a_esquerda()
->>> carro.calcular_a_direcao()
->>> 'Norte'
+>>> carro.calcular_direcao()
+'Norte'
 
 >>> carro.girar_a_esquerda()
->>> carro.calcular_a_direcao()
->>> 'Oeste'
+>>> carro.calcular_direcao()
+'Oeste'
 """
 class Motor:
     def __init__(self):
@@ -102,6 +102,24 @@ class Motor:
     def frear(self):
         self.velocidade-=2
         self.velocidade=max(0, self.velocidade)
+class Carro:
+    def __init__(self, direcao, motor):
+        self.motor = motor
+        self.direcao = direcao
+    def calcular_velocidade(self):
+        return self.motor.velocidade
+    def calcular_acelerar(self):
+        self.motor.acelerar()
+    def calcular_frear(self):
+        self.motor.frear()
+    def calcular_direcao(self):
+        return self.direcao.valor
+    def girar_a_direita(self):
+        self.direcao.girar_a_direita()
+    def girar_a_esquerda(self):
+            self.direcao.girar_a_esquerda()
+
+
 
 NORTE ='Norte'
 SUL   ='Sul'
