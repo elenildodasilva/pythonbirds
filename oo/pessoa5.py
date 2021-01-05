b8c6a6e131034d7ddf32c578b814285fc7931738
Elenildo <elenildo.silva@gmail.com>
2021-01-05 09:47:19 -0400

    criado herança e polimorfismo

--- oo/pessoa5.py
+++ oo/pessoa5.py
@@ -5,7 +5,7 @@ class Pessoa:
         self.idade = idade
         self.filhos = list(filhos)
     def cumprimentar (self):
-        return f'olá {id(self)}'
+        return f'olá, meu nome é  {self.nome}'
     @staticmethod
     def metodo_estatico():
         return 42
@@ -14,11 +14,17 @@ class Pessoa:
         return f'{cls} - olhos {cls.olhos}'
 
 class Homem(Pessoa):
-    pass
+    def cumprimentar(self):
+        cumprimentar_da_classe=super().cumprimentar()
+        return f'{cumprimentar_da_classe}. Aperto de mão'
+
+
+class Mutante(Pessoa):
+    olhos = 3
 
 if __name__== '__main__':
-    Pedro = Homem(nome='Pedro')
-    Elenildo = Pessoa(Pedro, nome='Elenildo')
+    Pedro = Mutante(nome='Pedro')
+    Elenildo = Homem(Pedro, nome='Elenildo')
     print(Pessoa.cumprimentar(Elenildo))
     print(id(Elenildo))
     print(Elenildo.cumprimentar())
@@ -32,7 +38,6 @@ if __name__== '__main__':
     del Elenildo.olhos
     print(Pedro.__dict__)
     print(Elenildo.__dict__)
-    Pessoa.olhos = 3
     print(Pessoa.olhos)
     print(Elenildo.olhos)
     print(id(Pessoa.olhos), id(Elenildo.olhos), id(Pedro.olhos))
@@ -43,6 +48,10 @@ if __name__== '__main__':
     print(isinstance(pessoa, Homem))
     print(isinstance(Pedro, Pessoa))
     print(isinstance(Pedro, Homem))
+    print(Pedro.olhos)
+    print(Elenildo.cumprimentar())
+    print(Pedro.cumprimentar())
+
 
 
 
